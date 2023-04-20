e3ebc69b665401088e5dc7ffcb44285b91496139
ashmalzahra <ashmal.zahraa@gmail.com>
2023-04-21 01:22:10 +0500

    Add corresponding code to pass the second test

--- solver.rb
+++ solver.rb
@@ -1,5 +1,6 @@
 class Solver
     def factorial(integer)
+        return 1 if integer.zero?
         raise StandardError, "Please insert a value greater than or equal to 0" if integer < 0
     end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,15 +2,15 @@ require_relative '../solver'
 
 describe Solver do
     before(:each) do
-      solver = Solver.new
+      @solver = Solver.new
     end
   
     describe '#factorial' do
       it 'should only accpect positive integers' do
-        expect { solver.factorial(-2) }.to raise_error(StandardError)
+        expect { @solver.factorial(-2) }.to raise_error(StandardError)
       end
       it 'should return 1 for given 0' do
-        expect(solver.factorial(0)).to eq 1
+        expect(@solver.factorial(0)).to eq 1
       end
     end
   end
