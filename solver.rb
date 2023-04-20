3a8ec4906a3d2e3b47204e55a572437793bd3a9d
ashmalzahra <ashmal.zahraa@gmail.com>
2023-04-21 01:25:11 +0500

    Add corresponding code to pass the third test

--- solver.rb
+++ solver.rb
@@ -2,5 +2,13 @@ class Solver
     def factorial(integer)
         return 1 if integer.zero?
         raise StandardError, "Please insert a value greater than or equal to 0" if integer < 0
+
+        value = 1
+        loop do
+            value *= integer
+            integer -= 1
+            break if integer.zero?
+        end
+        value
     end
 end
