fff4f08519a2cec2a45f2948b64d029f7e9c1a7e
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-04-21 03:21:57 +0600

    Added a method fizzbuzz and corresponding code to pass the test #fizzbuzz

--- solver.rb
+++ solver.rb
@@ -1,18 +1,25 @@
+# frozen_string_literal: true
+
+# created Class solver
 class Solver
-    def factorial(integer)
-        return 1 if integer.zero?
-        raise StandardError, "Please insert a value greater than or equal to 0" if integer < 0
+  def factorial(integer)
+    return 1 if integer.zero?
+    raise StandardError, 'Please insert a value greater than or equal to 0' if integer.negative?
 
-        value = 1
-        loop do
-            value *= integer
-            integer -= 1
-            break if integer.zero?
-        end
-        value
+    value = 1
+    loop do
+      value *= integer
+      integer -= 1
+      break if integer.zero?
     end
+    value
+  end
 
-    def reverse(word)
-        word.reverse
-    end
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(int)
+    return 'fizz' if (int % 3).zero?
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,31 +1,33 @@
+# frozen_string_literal: true
+
 require_relative '../solver'
 
 describe Solver do
-    before(:each) do
-      @solver = Solver.new
-    end
-  
-    describe '#factorial' do
-      it 'should only accpect positive integers' do
-        expect { @solver.factorial(-2) }.to raise_error(StandardError)
-      end
-      it 'should return 1 for given 0' do
-        expect(@solver.factorial(0)).to eq 1
-      end
-      it 'should return 24 for given integer 4' do
-        expect(@solver.factorial(4)).to eq 24
-      end
-    end
+  before(:each) do
+    @solver = Solver.new
+  end
 
-    describe '#reverse' do
-      it 'should return reverse of the given string' do
-        expect(@solver.reverse('Hello')).to eql ('olleH')
-      end
+  describe '#factorial' do
+    it 'should only accpect positive integers' do
+      expect { @solver.factorial(-2) }.to raise_error(StandardError)
     end
-
-    describe '#fizzbuzz ' do
-      it 'sould take an integer argument N and return a string "fizz"  if N is divisible by 3' do
-        expect (@solved.fizzbuzz(3).to eql ('fizz'))
-      end
+    it 'should return 1 for given 0' do
+      expect(@solver.factorial(0)).to eq 1
+    end
+    it 'should return 24 for given integer 4' do
+      expect(@solver.factorial(4)).to eq 24
     end
   end
+
+  describe '#reverse' do
+    it 'should return reverse of the given string' do
+      expect(@solver.reverse('Hello')).to eql('olleH')
+    end
+  end
+
+  describe '#fizzbuzz ' do
+    it 'sould take an integer argument N and return a string "fizz"  if N is divisible by 3' do
+      expect(@solver.fizzbuzz(3)).to eql('fizz')
+    end
+  end
+end
