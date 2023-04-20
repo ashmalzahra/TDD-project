08b8c563b19953ad47aeacb975654ec95ac8dbfa
ashmalzahra <ashmal.zahraa@gmail.com>
2023-04-21 01:01:38 +0500

    Add first test for fatorial to raise error for negative value

---
+++ solver_spec.rb
@@ -0,0 +1,13 @@
+require './solver'
+
+describe Solver do
+    before(:each) do
+      @solver = Solver.new
+    end
+  
+    describe '#factorial' do
+      it 'should only accpect positive integers' do
+        expect { solver.factorial(-2) }.to raise_error(StandardError)
+      end
+    end
+  end
